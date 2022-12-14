2ca62b3c73333dfd776aed7bb283cee2918e6585
pratik2630 <pratikkorgaonkar26@gmail.com>
2022-12-15 03:37:33 +0530

    Form project for hooks

---
+++ formapp/src/components/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+return <form>
+    <input type="text" name = "n" placeholder="Enter name" />
+    <br />
+    <input type="text" name = "p" placeholder="Enter phone" />
+    <br />
+    <input type="password" name= "passwd" placeholder="Enter password" />
+    <br />
+</form>
+}
+
+export default App
---
+++ formapp/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { App } from './components/App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.createRoot.render(
+  
+    <App />
+  ,document.getElementById('root')
+);
+
+
