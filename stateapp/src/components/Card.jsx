bbac95777a3854d2ac4fbe2cd3db31e2844447cd
pratik2630 <pratikkorgaonkar26@gmail.com>
2022-12-15 01:58:52 +0530

    State app

---
+++ stateapp/src/components/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+    return (
+        <>
+        <h1>Hello</h1>
+        </>
+    )
+}
+
+export {Card}
---
+++ stateapp/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import { Card } from './components/Card';
+
+
+ReactDOM.render(
+  <div>
+    <Card />
+    <button> click me</button>
+  </div>,
+  document.getElementById("root")
+)
