6c3be2e5615886d0acda2866bb34311f28b74398
pratik2630 <pratikkorgaonkar26@gmail.com>
2022-12-15 02:01:58 +0530

    DOM manipulation for changing color

--- stateapp/src/index.js
+++ stateapp/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 import { Card } from './components/Card';
 
-
+function colorChange(){
+  console.log("hii")
+  document.getElementById("root").style.color = "red";
+}
 ReactDOM.render(
   <div>
     <Card />
-    <button> click me</button>
+    <button onClick={colorChange}> click me</button>
   </div>,
   document.getElementById("root")
 )
